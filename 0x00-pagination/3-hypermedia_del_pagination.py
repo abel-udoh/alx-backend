189c77a457e743a44306f986a3be2d93954629f8
abel-udoh <abeludoh8@gmail.com>
2023-10-25 02:45:46 -0700

    pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+""" Deletion-resilient hypermedia pagination
 """
-Deletion-resilient hypermedia pagination
-"""
+
 
 import csv
 import math
-from typing import List
+from typing import List, Dict, Any
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """ Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -18,7 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """ Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -29,7 +29,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
+        """ Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -59,4 +59,4 @@ class Server:
             "data": data,
             "page_size": page_size,
             "next_index": _next
-            }
+        }
